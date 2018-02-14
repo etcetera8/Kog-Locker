1ceec31533a0b3801a06f77c53c7b0afb1c87316
parker lindley <fugazi8@gmail.com>
2018-02-13 22:52:16 -0700

    Add all stub tests for api

---
+++ src/api.test.js
@@ -0,0 +1,60 @@
+import { initialCall, segmentCall, statsCall, activitiesCall } from './api.js';
+
+describe('API calls', () => {
+
+  describe('initialCall', () => {
+    it('should be called with the right parameters', () => {
+
+    })
+
+    it('should return an object with an ok status', () => {
+
+    })
+
+    it('should return an error message if that status is not ok', () => {
+
+    })
+  })
+
+  describe('initialCall', () => {
+    it('should be called with the right parameters', () => {
+
+    })
+
+    it('should return an object with an ok status', () => {
+
+    })
+
+    it('should return an error message if that status is not ok', () => {
+
+    })  
+  })
+
+  describe('initialCall', () => {
+    it('should be called with the right parameters', () => {
+
+    })
+
+    it('should return an object with an ok status', () => {
+
+    })
+
+    it('should return an error message if that status is not ok', () => {
+
+    })
+  })
+
+  describe('initialCall', () => {
+    it('should be called with the right parameters', () => {
+
+    })
+
+    it('should return an object with an ok status', () => {
+
+    })
+
+    it('should return an error message if that status is not ok', () => {
+
+    })
+  })
+})
