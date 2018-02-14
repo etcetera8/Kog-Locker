e708cd2136b16d1838ec234f00f251ff5a459c5f
parker lindley <fugazi8@gmail.com>
2018-02-13 22:56:20 -0700

    Add cleaner test stubs

---
+++ src/cleaner.test.js
@@ -0,0 +1,19 @@
+import { cleanUser, cleanSegment, cleanStats, cleanActivities } from './cleaner.js';
+
+describe('cleaner functions', () => {
+  it('cleanUser should return a cleaned user', () => {
+
+  })
+
+  it('cleanSegment should return a cleaned segment', () => {
+
+  })
+
+  it('cleanStats should return cleaned stats', () => {
+
+  })
+
+  it('cleanActivities should return an array of cleaned activities', () => {
+
+  })
+})
