8fafdd511007c52866f580baadbb9d8222daf796
parker lindley <fugazi8@gmail.com>
2018-02-15 10:35:56 -0700

    Display all ytd data to card

--- src/components/StatsCard/StatsCard.js
+++ src/components/StatsCard/StatsCard.js
@@ -5,11 +5,14 @@ const StatsCard = ({yearStats}) => {
   console.log(yearStats);
   return (
     <section className="stats-card card">
-      <h2>Y-T-D Stats</h2>
+      <h2 className="title">Year To Data</h2>
       { yearStats &&
-        <span>{yearStats.count}</span>
-        <span>{yearStats.distance}</span>
-        <span>{yearStats.}</span>
+        <div>
+          <span className="card-data">Rides: {yearStats.count}</span>
+          <span className="card-data">Disitance: {yearStats.distance}</span>
+          <span className="card-data">Elevation: {yearStats.elevation_gain}</span>
+          <span className="card-data">Time: {yearStats.elapsed_time}</span>
+        </div>
       }
     </section>
   )
