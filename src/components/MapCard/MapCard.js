89b8e27f523baa7e0579b6a843e219b3836ec222
parker lindley <fugazi8@gmail.com>
2018-02-17 12:33:28 -0700

    Add data to target card

--- src/components/MapCard/MapCard.js
+++ src/components/MapCard/MapCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './MapCard.css'
 const codedPath = "_xpqF`hgaS\QHDFJCNSb@Ul@Q`AY|BA\Dl@Rt@PdA@f@E\IRKJuAr@UXSr@SpAYr@U\o@r@yAdAw@p@gAlAS^Kb@An@Jx@Nz@?h@SVIBa@EcBy@g@Oq@Ac@Je@^S`@G^?v@Bj@R`B@ZKZKB[C{@g@a@Oa@I]A]Bk@R_@\{@nAYXSNa@LaACWHGDU\Mj@EvAKd@[`@UDcC@_@Dk@PUNOPIN]hAm@xAEN@~@NxAHb@P^x@hAXv@XdBNnAB|@Eh@]bBI~@CvAO|B?HJ^H@Z[p@iEb@gBFg@D}ABERAHV@n@Ct@GvAOnB@LPVJ?RODYHwAh@wCDq@@g@C_AQu@eAcBQi@Gg@@oAFe@HUVWXGn@Bl@@XIbAo@VIZAn@Db@EZQnAiA\SRGZC`@@ZFzAXh@PPNZd@^^\NnA^XFZ@\OJKz@_B`@WPAJ@NDx@t@|Af@`@`@Vp@FTnA|GRp@Xh@pAlAX`@ZZRFV@^El@QTCJ@\JdAz@`@`@V^x@tA\r@xApDX^LHLBJ?VGPM`AeAf@e@p@e@d@Sd@?pANfDVpAArAI\?JL@LEHKJYDm@Ec@BM`@JPVHr@A`@BDF@PEJSJq@Ae@I]CwBFoBPWFW`@?LHPVLT?fAM`@@d@LzAz@VFZBn@Cr@Un@_@hBoBZ_@Rk@Jo@L{BLs@HWLSn@e@LMHg@Si@u@s@i@]uAo@]WK]IiBWgBDe@Z{@?g@GKSQm@S_Ag@cBc@U[C_@H{AHe@N]jCoDh@m@POVKTGt@Ah@Br@JPFHHXf@DRBPAr@ShCCp@@h@"
 export default class MapCard extends Component {
 
@@ -7,7 +8,7 @@ export default class MapCard extends Component {
     await this.loadMap();
   }
 
-  async loadMap () {
+  async loadMap (polyline, latLong) {
     if (this.props && this.props.google.maps.geometry.encoding) {
       const {google} = this.props;
       const maps = google.maps;
@@ -41,15 +42,27 @@ export default class MapCard extends Component {
   }
 
   render() {
+    const {name, athlete_segment_stats} = this.props.userTarget
+    console.log(this.props.userTarget, athlete_segment_stats);
+    
     const style = {
       width: '200px',
       height: '200px',
     }
 
     return (
-   
-        <div id="map" className='map card' ref='map' style={style}>
-        </div>
+        <section className='card map-card'>
+          {
+            athlete_segment_stats && 
+            <div>
+              <h3 className="target-name">{name}</h3>
+            
+              <div id="map" className='map' ref='map' style={style}>
+              </div>
+              <span>Best Time: {(athlete_segment_stats.pr_elapsed_time / 60)} minutes</span>
+            </div>
+          }
+        </section>
       
     )
   }
