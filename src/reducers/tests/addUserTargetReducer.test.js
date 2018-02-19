c4200a73c6cd96ae0f81743f3163f95da5fc29c1
parker lindley <fugazi8@gmail.com>
2018-02-18 18:18:12 -0700

    Add user target reducer test

---
+++ src/reducers/tests/addUserTargetReducer.test.js
@@ -0,0 +1,14 @@
+import * as actions from '../../actions/actionIndex';
+import { addUserTarget } from '../addUserTargetReducer';
+
+describe('addUserTargetReducer', () => {
+  it('should return a default state of an empty object', () => {
+    expect(addUserTarget(undefined, {})).toEqual({})
+  })
+
+  it('should return an object of a target segment', () => {
+    const expected = {"payload": {"name": "Lookout Mountain"}, "type": "ADD_USER_TARGET"}
+    const data = {type: 'ADD_USER_TARGET', payload: {name:'Lookout Mountain'}}
+    expect(addUserTarget(undefined, actions.addUserTarget(data))).toEqual(expected);
+  })
+})
