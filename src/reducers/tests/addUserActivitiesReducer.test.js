6162304eb3ec26e98db670334d22dba685786234
parker lindley <fugazi8@gmail.com>
2018-02-18 17:46:21 -0700

    Add useractivity reducer tests

---
+++ src/reducers/tests/addUserActivitiesReducer.test.js
@@ -0,0 +1,13 @@
+import * as actions from '../../actions/actionIndex';
+import { addUserActivities } from '../addUserActivitiesReducer';
+
+describe('User Activities reducer', () => {
+  it('should return a default state of an empty array', () => {
+    expect(addUserActivities(undefined, {})).toEqual([]);
+  })
+
+  it('should return a an array of activites if called', () => {
+    const expected = [{activity: 1234}]
+    expect(addUserActivities(undefined, {type: 'ADD_USER_ACTIVITIES', payload:expected})).toEqual(expected)
+  })
+})
