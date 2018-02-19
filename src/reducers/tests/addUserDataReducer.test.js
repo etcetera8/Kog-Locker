361bc085db7868de9e2321fd6c27dc40206d700b
parker lindley <fugazi8@gmail.com>
2018-02-18 17:54:40 -0700

    add data reducer tests

---
+++ src/reducers/tests/addUserDataReducer.test.js
@@ -0,0 +1,13 @@
+import * as actions from '../../actions/actionIndex';
+import { addUserData } from '../addUserDataReducer';
+
+describe('user data reducer', () => {
+  it('should return a default state of an empty object', () => {
+    expect(addUserData(undefined, {})).toEqual({});
+  })
+
+  it('should return a default state of an empty object', () => {
+    const expected = {name: 'parker'}
+    expect(addUserData({}, actions.addUserData({name: 'parker'}))).toEqual(expected);
+  })
+})
