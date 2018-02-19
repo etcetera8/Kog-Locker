c45cf896bb72af3436d9bcd816fe3c67abe00346
parker lindley <fugazi8@gmail.com>
2018-02-18 17:58:28 -0700

    Add tests for stats reducer

---
+++ src/reducers/tests/addUserStatsReducer.test.js
@@ -0,0 +1,13 @@
+import * as actions from '../../actions/actionIndex';
+import { addUserStats } from '../addUserStatsReducer';
+
+describe('AddUserStatsReducer', () => {
+  it('should return a default state of an empty object', () => {
+    expect(addUserStats(undefined, {})).toEqual({})
+  })
+
+  it('should return an object of stats when passed an action', () => {
+    const stats = { max_power: 1000 };
+    expect(addUserStats(undefined, actions.addUserStats(stats))).toEqual(stats);
+  })
+})
