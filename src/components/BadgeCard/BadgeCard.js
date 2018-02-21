d3ec42ba2ebffd6dd12d90449fe67c66423d940d
parker lindley <fugazi8@gmail.com>
2018-02-21 11:43:29 -0700

    Succesful experiment, refactor badge to being connected to store

--- src/components/BadgeCard/BadgeCard.js
+++ src/components/BadgeCard/BadgeCard.js
@@ -3,30 +3,15 @@ import Badge from '../Badge/Badge';
 import './Badge.css';
 import PropTypes from 'prop-types';
 
-const BadgeCard = ({currentEl, goalEl, goalDist, allGoalDist, allGoalEl, yearStats, allStats}) => {
+const BadgeCard = () => {
   return (
     <section className="badge-card card">
       <h2 className='title'> Badges </h2>
       <div className='badge-card-inner'>
-        <Badge 
-          currentEl={currentEl}
-          goalEl={goalEl}
-          goalDist={goalDist}
-          allGoalDist={allGoalDist}
-          allGoalEl={allGoalEl}
-          yearStats={yearStats}
-          allStats={allStats}
-        />
+        <Badge />
       </div>
     </section>
   );
-};
-
-BadgeCard.propTypes = {
-  currentEl: PropTypes.number.isRequired,
-  goalEl: PropTypes.number.isRequired,
-  goalDist: PropTypes.number.isRequired,
-  yearStats:PropTypes.object.isRequired
 };
 
 export default BadgeCard;
