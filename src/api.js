487aadc389eb085b047d924128c369a466111f6f
parker lindley <fugazi8@gmail.com>
2018-02-28 13:24:30 -0700

    lazy load working

--- src/api.js
+++ src/api.js
@@ -59,5 +59,4 @@ export const photosCall = async(id, num) => {
   });
 
   return photoArray;
-  
 };
--- src/components/PhotoGallery/PhotoGallery.js
+++ src/components/PhotoGallery/PhotoGallery.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './PhotoGallery.css';
 import { LazyBackgroundImage, LazyImage, LazyFrame } from "lazy-react";
+import { photosCall } from '../../api';
 
 export class PhotoGallery extends Component {
   constructor() {
@@ -15,18 +16,36 @@ export class PhotoGallery extends Component {
   //   this.removeEventListener('scroll', this.onScroll, false)
   // }
 
-  onScroll = (event) => {
-    console.log(this)
-  }
+  // onScroll = (event) => {
+  //   console.log(this)
+  // }
 
   photos = () => this.props.photoArray.map( (url, i) => {
     return <LazyImage onScroll = {this.onScroll} offset={1} className="photo" key={i} link={url} offset={100} />;
   });
+
+  isInViewport() {
+    var rect = document.querySelector('.more-photos').getBoundingClientRect();
+    var html = document.documentElement;
+    const inView = (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+    console.log(inView);
+    if (inView === true) {
+      console.log('fire fetch');
+      this.props.lazyLoad(); 
+    }
+    return inView;
+    
+  }
   
   render() {
     return (
       <div>
-        <div onScroll={this.onScroll} className='gallery'>
+        <div onScroll={() => this.isInViewport()} className='gallery'>
           { this.photos() }
           <button className="more-photos" onClick={this.props.lazyLoad}>Load more</button>
         </div>
